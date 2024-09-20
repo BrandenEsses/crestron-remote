1aca6e0b79cf45bfdfdaf416ef1d153494601fc1
Branden Lee Esses <branden.esses@colorado.edu>
2024-09-20 17:34:34 -0600

    Finishing source switching

--- app.py
+++ app.py
@@ -14,29 +14,8 @@ def init_crestron_connection():
     living_room_cip.start()
     time.sleep(1.5)
 
-def init_adb_connection():
-    global device
-    if not os.path.isfile(ADB_KEY_PATH):
-        keygen(ADB_KEY_PATH)
-    with open(ADB_KEY_PATH) as f:
-        priv = f.read()
-    with open(ADB_KEY_PATH + '.pub') as f:
-        pub = f.read()
-    signer = PythonRSASigner(pub, priv)
-    device = AdbDeviceTcp(CHROMECAST_IP, CHROMECAST_ADB_PORT, default_transport_timeout_s=9.)
-    device.connect(rsa_keys=[signer], auth_timeout_s=0.1)
-
-def init_samsung_connection():
-    global tv
-    if not os.path.isfile(BEDROOM_TV_TOKEN_FILE_PATH):
-        with open(BEDROOM_TV_TOKEN_FILE_PATH, 'w') as fp:
-            pass
-    tv = SamsungTVWS(host=BEDROOM_TV_IP, port=BEDROOM_TV_PORT, token_file=BEDROOM_TV_TOKEN_FILE_PATH)
-
 with app.app_context():
     init_crestron_connection()
-    # init_adb_connection()
-    # init_samsung_connection()
 
 @app.route('/')
 def index():
@@ -53,44 +32,6 @@ def bedroom():
 @app.route('/living_room_join_handler', methods = ['POST'])
 def living_room_join_handler():
     button_data = request.args.get("button")
-    if "adb" in button_data:
-        if "vol_up" in button_data:
-            response = device.shell('input keyevent "KEYCODE_VOLUME_UP"')
-        if "vol_down" in button_data:
-            response = device.shell('input keyevent "KEYCODE_VOLUME_DOWN"')
-        if "vol_mute" in button_data:
-            response = device.shell('input keyevent "KEYCODE_VOLUME_MUTE"')
-        return ("nothing")
-    if 'source_select' in button_data and not living_room_cip.get("d",59):
-        response = device.shell('input keyevent "KEYCODE_POWER"')
-        time.sleep(0.25)
-    button_join = JOIN_DICT[button_data]
-    living_room_cip.pulse(button_join)
-    return ("nothing")
-
-@app.route('/bedroom_join_handler', methods = ['POST'])
-def bedroom_join_handler():
-    button_data = request.args.get("button")
-    try:
-        if 'bedroom_tv' in button_data:
-            if 'vol_up' in button_data:
-                tv.shortcuts().volume_up()
-            if 'vol_down' in button_data:
-                tv.shortcuts().volume_down()
-            if 'vol_mute' in button_data:
-                tv.shortcuts().mute()
-            if 'up' in button_data:
-                tv.shortcuts().up()
-            if 'down' in button_data:
-                tv.shortcuts().down()
-            if 'left' in button_data:
-                tv.shortcuts().left()
-            if 'right' in button_data:
-                tv.shortcuts().right()
-            if 'select' in button_data:
-                tv.shortcuts().enter()
-    except:
-        init_samsung_connection()
     button_join = JOIN_DICT[button_data]
     living_room_cip.pulse(button_join)
     return ("nothing")
