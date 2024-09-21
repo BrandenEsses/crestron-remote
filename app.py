368dd2995b12158a7ae3f696d830556ccbaa7a4d
Branden Lee Esses <branden.esses@colorado.edu>
2024-09-21 12:37:23 -0600

    Added light controls

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import cipclient,time,os
 from config import *
+from huesdk import Hue
 from adb_shell.adb_device import AdbDeviceTcp
 from adb_shell.auth.sign_pythonrsa import PythonRSASigner
 from adb_shell.auth.keygen import keygen
@@ -8,6 +9,10 @@ from samsungtvws import SamsungTVWS
 
 app = Flask(__name__, template_folder='templates')
 
+def init_hue_sdk():
+    hue = Hue(bridge_ip=HUE_BRIDGE_IP, username=HUE_USERNAME)
+    global groups
+    groups = hue.get_groups()
 def init_crestron_connection():
     global living_room_cip
     living_room_cip = cipclient.CIPSocketClient(CONTROL_PROCESSOR_HOST, LIVING_ROOM_PANEL_IPID)
@@ -16,6 +21,7 @@ def init_crestron_connection():
 
 with app.app_context():
     init_crestron_connection()
+    init_hue_sdk()
 
 @app.route('/')
 def index():
@@ -34,6 +40,19 @@ def living_room_join_handler():
     button_data = request.args.get("button")
     button_join = JOIN_DICT[button_data]
     living_room_cip.pulse(button_join)
+    return ("nothing")
+
+@app.route('/hue', methods = ['POST'])
+def hue():
+    button_data = request.args.get("button")
+    for i in range(len(groups)):
+        if(groups[i].name == "Living Room"):
+            living_room_group = groups[i]
+    if(button_data=="living_room_off"):
+        living_room_group.off()
+    if(button_data=="living_room_on"):
+        living_room_group.on()
+        living_room_group.set_brightness(254)
     return ("nothing")
 
 if __name__ == '__main__':
