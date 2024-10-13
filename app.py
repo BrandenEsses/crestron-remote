edb68f60e170905821b182978c240886800bfff6
Branden Lee Esses <branden.esses@colorado.edu>
2024-10-12 20:35:05 -0600

    Added syncing current Hue light state

--- app.py
+++ app.py
@@ -1,14 +1,38 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import cipclient,time,os
 from config import *
 from huesdk import Hue
-from adb_shell.adb_device import AdbDeviceTcp
-from adb_shell.auth.sign_pythonrsa import PythonRSASigner
-from adb_shell.auth.keygen import keygen
-from samsungtvws import SamsungTVWS
 
 app = Flask(__name__, template_folder='templates')
 
+
+def hsv_to_rgb(H, S, V):
+    C = V * S  # Chroma
+    H_prime = H / 60.0
+    X = C * (1 - abs(H_prime % 2 - 1))
+    m = V - C
+
+    if 0 <= H_prime < 1:
+        r1, g1, b1 = C, X, 0
+    elif 1 <= H_prime < 2:
+        r1, g1, b1 = X, C, 0
+    elif 2 <= H_prime < 3:
+        r1, g1, b1 = 0, C, X
+    elif 3 <= H_prime < 4:
+        r1, g1, b1 = 0, X, C
+    elif 4 <= H_prime < 5:
+        r1, g1, b1 = X, 0, C
+    elif 5 <= H_prime < 6:
+        r1, g1, b1 = C, 0, X
+    else:
+        r1, g1, b1 = 0, 0, 0
+
+    # Convert to RGB values between 0 and 255
+    R = int(round((r1 + m) * 255))
+    G = int(round((g1 + m) * 255))
+    B = int(round((b1 + m) * 255))
+
+    return R, G, B
 def init_hue_sdk():
     hue = Hue(bridge_ip=HUE_BRIDGE_IP, username=HUE_USERNAME)
     global groups
@@ -42,12 +66,26 @@ def living_room_join_handler():
     living_room_cip.pulse(button_join)
     return ("nothing")
 
-@app.route('/hue', methods = ['POST'])
+@app.route('/hue', methods = ['GET','POST'])
 def hue():
     button_data = request.args.get("button")
     for i in range(len(groups)):
         if(groups[i].name == "Living Room"):
             living_room_group = groups[i]
+    light = living_room_group.get_lights()[0]
+    if(request.method == 'GET'):
+        args = list(request.args.keys())
+        if("status" in args):
+            current_bri = light.__getattribute__("bri")
+            current_hue = light.__getattribute__("hue")
+            current_sat = light.__getattribute__("sat")
+            H = (current_hue / 65535.0) * 360.0
+            S = current_sat / 254.0
+            V = current_bri / 254.0
+            R, G, B = hsv_to_rgb(H, S, V)
+            hex_color = '#{0:02X}{1:02X}{2:02X}'.format(R, G, B)
+            return jsonify({"brightness": current_bri,"hue":hex_color})
+
     if(request.args.get("brightness")):
         brightness = int(request.args.get("brightness"))
         living_room_group.set_brightness(brightness)
@@ -59,7 +97,7 @@ def hue():
         living_room_group.off()
     if(button_data=="living_room_on"):
         living_room_group.on()
-        living_room_group.set_color(hexa="#f5d269")
+        living_room_group.set_color(hexa="#F7AD45")
         living_room_group.set_brightness(254)
     return ("nothing")
 
